c65fda4d7a236927df0219a0e49c1da161233e2e
Tushar Shah <twoshark@gmail.com>
2020-09-06 14:09:38 -0400

    remove secret

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define OFFSET_FIXTURE 0 //DMX Start Channel
 
 //Sensitive Config
-#define WIFI_SSID "The Quiet Neighbor"
-#define PASSWORD "AllAboutThatBase"
+#define WIFI_SSID "secret"
+#define PASSWORD "secret"
 
 #endif
