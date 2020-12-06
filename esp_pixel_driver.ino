48a4653b356992e7b6dd7008d203943298bdd2e2
Tushar Shah <twoshark@gmail.com>
2020-12-05 21:08:56 -0500

    mdns

--- esp_pixel_driver.ino
+++ esp_pixel_driver.ino
@@ -6,6 +6,7 @@
 #include <Artnet.h>
 #include <Adafruit_NeoPixel.h>
 #include <esp8266wifi.h>
+#include <ESP8266mDNS.h>        // Include the mDNS library
 
 ArtnetWiFiReceiver artnet;
 Ota ota;
@@ -23,30 +24,31 @@ Ota ota;
 //Adafruit_NeoPixel gpio10 = Adafruit_NeoPixel(STRIP_LENGTH, 10, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel gpio11 = Adafruit_NeoPixel(STRIP_LENGTH, 11, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel gpio12 = Adafruit_NeoPixel(STRIP_LENGTH, 12, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel gpio13 = Adafruit_NeoPixel(STRIP_LENGTH, 13, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel gpio13 = Adafruit_NeoPixel(STRIP_LENGTH, 13, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel gpio14 = Adafruit_NeoPixel(STRIP_LENGTH, 14, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel gpio15 = Adafruit_NeoPixel(STRIP_LENGTH, 15, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel gpio16 = Adafruit_NeoPixel(STRIP_LENGTH, 16, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel *pixels_mapped = &gpio13;
-PixelMappedOutput pmo(pixels_mapped);
-
-//Network Config
-const IPAddress ip(192, 168, 1, 201);
-const IPAddress gateway(192, 168, 1, 1);
-const IPAddress subnet(255, 255, 255, 0);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(STRIP_LENGTH, 13, NEO_GRB + NEO_KHZ800);
+PixelMappedOutput pmo(&pixels);
 
 void setup_wifi()
 {
   WiFi.begin(WIFI_SSID, PASSWORD);
-  WiFi.config(ip, gateway, subnet);
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print("-~~<*}(~){*>~~-\n");
+    Serial.println("-~~<*}(~){*>~~-\n");
     delay(500);
   }
-  Serial.print("WiFi connected, IP = ");
+  Serial.println("WiFi connected, IP = ");
   Serial.println(WiFi.localIP());
+  delay(500);
+  //Multicast DNS
+  if (!MDNS.begin("idplaceholder.pixel.driver")) {             
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+  
 }
 
 void pixel_mapping_subscriber(uint8_t *data, uint16_t size)
@@ -59,9 +61,10 @@ void setup()
   Serial.begin(115200);
   setup_wifi();
 
+  pixels.begin();
+  pixels.clear();
+  
   artnet.begin();
-  pixels_mapped->begin();
-  pixels_mapped->clear();
   artnet.subscribe(UNIVERSE_PIXEL_MAPPED, pixel_mapping_subscriber);
 }
 
